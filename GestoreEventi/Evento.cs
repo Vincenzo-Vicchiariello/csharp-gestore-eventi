cec326d96770ff77e853143a9cb93f7146052696
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-28 10:42:01 +0200

    Iniziato esercizio con classe, evento e setter che chiede all'utente di inserire un nome per l'evento da console.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Event
+    {
+        private string title = string.Empty;
+        private string date = DateTime.Today.ToString("yy-MM-yyyy");
+        private int maxCapacity = 0;
+        private int reservedSpots = 0;
+
+    public Event(string title, string date, int maxCapacity, int reservedSpots)
+    {
+            this.title = title;
+            this.date = date;
+            this.maxCapacity = maxCapacity;
+            this.reservedSpots = reservedSpots;
+    }
+        
+
+
+        public string setTitle()
+        {
+            this.title = Console.ReadLine();
+            this.title = title;
+
+
+            return title;
+        
+          }
+
+        public override string ToString()
+        {
+            string eventInfo = $"Nome dell'evento: {title} \n" +
+                                $"Data: {date}\n"+
+                                $"Capienza massima: {maxCapacity}\n"+
+                                $"Prenotazioni effettuate: {reservedSpots}";
+            return eventInfo;
+
+        }
+
+    }
+
+}
---
+++ GestoreEventi/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GestoreEventi;
+
+Event Event1 = new Event("Salviamo le capocchie","26/05/2024", 50, 0);
+
+Console.WriteLine(Event1);
+
+Event1.setTitle();
+
+Console.WriteLine(Event1);
